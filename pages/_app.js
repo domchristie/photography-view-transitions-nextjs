84afd26b835971f82f0ebebc8bb5027dbf22da54
Dom Christie <christiedom@gmail.com>
2023-03-16 21:38:53 +0000

    Setup basic view transition

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import ViewTransition from '@/view-transition'
+import Link from 'next/link'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ViewTransition>
+      <nav>
+        <Link href="/" >Home</Link>
+        <Link href="/about" >About</Link>
+      </nav>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </ViewTransition>
+  )
 }
---
+++ view-transition.js
@@ -0,0 +1,30 @@
+import { Component} from 'react'
+
+export default class ViewTransition extends Component {
+  shouldComponentUpdate () {
+    if (!document.startViewTransition) return true // skip when not supported
+
+    document.startViewTransition(() => this.#updateDOM())
+    return false // don't update the component, we'll do this manually
+  }
+
+  #updateDOM () {
+    // now we know the screenshot has been taken, we can force render
+    // (which skips `shouldComponentUpdate`)
+    this.forceUpdate()
+    // set up a promise that will resolve when the component renders
+    return new Promise(resolve => { this.#rendered = resolve })
+  }
+
+  render () {
+    return this.props.children
+  }
+
+  #rendered = () => {}
+
+  componentDidUpdate () {
+    // resolve the `updateDOM` promise to notify the View Transition API
+    // that the DOM has been updated
+    this.#rendered()
+  }
+}
